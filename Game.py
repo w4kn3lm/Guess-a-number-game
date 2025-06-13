22ea8d5d8515ea4abd52f590d91cfc8d2c6bd213
ki241_fao@student.ztu.edu.ua <ki241_fao@student.ztu.edu.ua>
2025-06-13 18:31:32 +0300

    Додав початкове меню та покращив вiзуальну складову

--- Game.py
+++ Game.py
@@ -1,74 +1,64 @@
 import random
 
-class MyClass:   
-    def game(self):
+class MyClass:
+    def main_menu(self):
         print('Пограємно в "Вгадай число" :)')
         level = input('Обери рівень складності від 1 до 3: ')
-        
+        self.game(level)
+
+    def game(self, level):
+        print('================================')
         if level == '1':
-          number = random.randint(1,10)
-          print('Я загадав рандомне число на проміжку від 1 до 10: ') 
-          att = 5
-          for i in range(0, 5):       
-            print(f'Залишилось спроб: {att}')
-            us_num = int(input(f'Введіть число: '))
-            att = att - 1
-            if us_num == number:
-              print('Вітаю! Ти вгадав!')
-              self.menu()
-            else:
-             if us_num > number:
-                 print('Не вгадав! Менше!')
-             else:
-                 print('Не вгадав! Більше!')
-            
-               
-        elif level == '2':
-            number = random.randint(1,25)
-            print('Я загадав рандомне число на проміжку від 1 до 25: ') 
+            number = random.randint(1, 10)
+            print('Я загадав число від 1 до 10.')
             att = 5
-            for i in range(0, 5):       
-              print(f'Залишилось спроб: {att}')
-              us_num = int(input(f'Введіть число: '))
-              att = att - 1
-              if us_num == number:
+        elif level == '2':
+            number = random.randint(1, 25)
+            print('Я загадав число від 1 до 25.')
+            att = 5
+        elif level == '3':
+            number = random.randint(1, 50)
+            print('Я загадав число від 1 до 50.')
+            att = 3
+        else:
+            print('Неправильний рівень. Спробуй ще раз.')
+            self.main_menu()
+            return
+
+        while att > 0:
+            print(f'Залишилось спроб: {att}')
+            try:
+                us_num = int(input('Введіть число: '))
+            except ValueError:
+                print("Це не число!")
+                continue
+
+            att -= 1
+            if us_num == number:
+                print('================================')
                 print('Вітаю! Ти вгадав!')
                 self.menu()
-              else:
-                if us_num > number:
-                  print('Не вгадав! Менше!')
-                else:
-                  print('Не вгадав! Більше!')
-
-        elif level == '3':
-          number = random.randint(1,50)
-          print('Я загадав рандомне число на проміжку від 1 до 50: ') 
-          att = 3
-          for i in range(0, 3):       
-           print(f'Залишилось спроб: {att}')
-           us_num = int(input(f'Введіть число: '))
-           att = att - 1
-           if us_num == number:
-             print('Вітаю! Ти вгадав!')
-             self.menu()
-          else:
-            if us_num > number:
+                return
+            elif us_num > number:
+                print('================================')
                 print('Не вгадав! Менше!')
             else:
+                print('================================')
                 print('Не вгадав! Більше!')
-        else:
-         print('Неправильно введене число')
-         
+
+        print(f'Ти не вгадав. Загадане число було: {number}')
+        self.menu()
+
     def menu(self):
-        ch = int(input('Бажаєш грати чи завершити гру? 1/2 - грати/завершити: '))
-        if ch == 1:
-           self.game()
-        elif ch == 2:
-           print('До побачення!')
-           exit()
+        ch = input('Бажаєш грати знову? 1 - так / 2 - вийти: ')
+        if ch == '1':
+            self.main_menu()
+        elif ch == '2':
+            print('До побачення!')
+            exit()
         else:
-          print('Неправильно введене число!')
-          self.menu()
+            print('Неправильне введення.')
+            self.menu()
 
 app = MyClass()
-app.menu()  
+app.main_menu()
